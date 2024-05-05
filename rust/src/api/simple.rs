25ec8adb6746aa44e4829723d127e97dfe3e9b8c
evilDAVE <aymanfarsi99@gmail.com>
2024-05-05 12:59:58 -0400

    chore: Comment out toast-related code in simple.rs

--- rust/src/api/simple.rs
+++ rust/src/api/simple.rs
@@ -9,18 +9,18 @@ pub fn greet(name: String) -> String {
 
 #[flutter_rust_bridge::frb(sync)]
 pub fn show_toast(message: String) {
-    let manager = ToastManager::new("AymanFarsi.Zyron");
+    // let manager = ToastManager::new("AymanFarsi.Zyron");
 
-    let mut toast = Toast::new();
-    toast
-        .text1("Zyron")
-        .text2(Text::new(message))
-        .text3(Text::new("evilDAVE").with_placement(TextPlacement::Attribution));
+    // let mut toast = Toast::new();
+    // toast
+    //     .text1("Zyron")
+    //     .text2(Text::new(message))
+    //     .text3(Text::new("evilDAVE").with_placement(TextPlacement::Attribution));
 
-    match manager.show(&toast) {
-        Ok(_) => {}
-        Err(e) => eprintln!("Error showing toast: {:?}", e),
-    }
+    // match manager.show(&toast) {
+    //     Ok(_) => {}
+    //     Err(e) => eprintln!("Error showing toast: {:?}", e),
+    // }
 }
 
 #[flutter_rust_bridge::frb(init)]
@@ -29,12 +29,12 @@ pub fn init_app() {
     flutter_rust_bridge::setup_default_user_utils();
 
     // Initialize the ToastManager
-    let icon_path = "assets/zyron_icon.png";
-    let obsolute_icon_path = PathBuf::from(icon_path).canonicalize().unwrap();
-    match register("AymanFarsi.Zyron", "Zyron", Some(obsolute_icon_path.as_path())) {
-        Ok(_) => {}
-        Err(e) => eprintln!("Error registering toast: {:?}", e),
-    }
+    // let icon_path = "assets/zyron_icon.png";
+    // let obsolute_icon_path = PathBuf::from(icon_path).canonicalize().unwrap();
+    // match register("AymanFarsi.Zyron", "Zyron", Some(obsolute_icon_path.as_path())) {
+    //     Ok(_) => {}
+    //     Err(e) => eprintln!("Error registering toast: {:?}", e),
+    // }
 
     println!("Rust app initialized!");
 }
